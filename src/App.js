befaa2e1dc0ca1e3edde23e2f093e694911bc83a
Shreya <shreyadenny5@gmail.com>
2023-08-21 13:40:58 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './views/Home';
 import CreateForm from './views/CreateForm';
 import FormList from './views/FormList';
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ function Header() {
   return (
     <header className='Header'>
     <nav className='Nav'>
-      <Link to="/">Home</Link>
       <Link to="/create-form">Create Form</Link>
       <Link to="/form-list">Form List</Link>
     </nav>
